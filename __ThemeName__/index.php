446743c6fc2260a0df80cb6aa9ad7223e8bdd767
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-03-16 15:09:23 +0100

    add a stub-theme for making new themes inside __ThemeName__ folder, run the ./maketheme command for instructions will create a new theme child of OsloMetParent

---
+++ __ThemeName__/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @defgroup plugins_themes___ThemeName__ __JournalName__ theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/__ThemeName__/index.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes___ThemeName__
+ * @brief Wrapper for __ThemeName__ theme plugin.
+ *
+ */
+
+require_once('__ThemeName__ThemePlugin.inc.php');
+
+return new __ThemeName__ThemePlugin();
+
+?>
+
