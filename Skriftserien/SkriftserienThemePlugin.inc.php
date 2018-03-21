5c5fb28cfcd7ab8b446b95f086b88d44633b7560
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-03-21 12:33:25 +0100

    new theme Skriftserien

---
+++ Skriftserien/SkriftserienThemePlugin.inc.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file plugins/themes/Skriftserien/SkriftserienThemePlugin.inc.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SkriftserienThemePlugin
+ * @ingroup plugins_themes_Skriftserien
+ *
+ * @brief Skriftserien theme
+ */
+
+import('lib.pkp.classes.plugins.ThemePlugin');
+
+class SkriftserienThemePlugin extends ThemePlugin {
+
+    /**
+     * Load the custom styles for our theme
+     * @return null
+     */
+    
+	public function init() {
+
+        // Use the parent theme's unique plugin slug
+        $this->setParent('oslometparentthemeplugin');
+
+        // Change the ID of this stylesheet slug to
+        // `child-stylesheet`. This ensures that it
+        // won't clash with the parent's stylesheet.
+        $this->modifyStyle('stylesheet', array('addLess' => array('styles/index.less')));
+	}
+	
+	/**
+	 * Get the display name of this plugin
+	 * @return string
+	 */
+	function getDisplayName() {
+		return __('plugins.themes.Skriftserien.name');
+	}
+
+	/**
+	 * Get the description of this plugin
+	 * @return string
+	 */
+	function getDescription() {
+		return __('plugins.themes.Skriftserien.description');
+	}
+}
---
+++ Skriftserien/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_Skriftserien Skriftserien theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/Skriftserien/index.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_Skriftserien
+ * @brief Wrapper for Skriftserien theme plugin.
+ *
+ */
+
+require_once('SkriftserienThemePlugin.inc.php');
+
+return new SkriftserienThemePlugin();
+
+?>
+
