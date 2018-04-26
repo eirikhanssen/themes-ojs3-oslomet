41b4b88410b1bbdfc54ca7e8178a961a60faecfc
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-04-26 11:13:53 +0200

    The language selection in the backend for english speakers who can't understand nordic languages is difficult to understand when a non-english language is selected. Add "Language: " before the language selection in the backend and style it slightly yellow to make it stand out and be more visible.

--- OsloMetParent/OsloMetParentThemePlugin.inc.php
+++ OsloMetParent/OsloMetParentThemePlugin.inc.php
@@ -25,7 +25,10 @@ class OsloMetParentThemePlugin extends ThemePlugin {
 	public function init() {
 
         // Use the parent theme's unique plugin slug
-        $this->setParent('defaultthemeplugin');
+		$this->setParent('defaultthemeplugin');
+
+		// add style overrides for the backend
+		$this->addStyle('backend-overrides', 'styles/backend.less', array( 'contexts' => 'backend' ));
 
         // Change the ID of this stylesheet slug to
         // `child-stylesheet`. This ensures that it
