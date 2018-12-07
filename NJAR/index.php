5fd1fe9cbca9d56ffead5944fe7f5b98fdbdf49a
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-12-07 12:54:09 +0100

    Merge new NJAR theme from topic branch

---
+++ NJAR/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_NJAR NJAR - Nordic Journal of Art and Research theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/NJAR/index.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_NJAR
+ * @brief Wrapper for NJAR theme plugin.
+ *
+ */
+
+require_once('NJARThemePlugin.inc.php');
+
+return new NJARThemePlugin();
+
+?>
+
